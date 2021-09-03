db8312f9eefaead98f6a5e2ec1b13789de91c06a
Shawn Soltesz <shawnjsoltesz@gmail.com>
2021-09-02 21:50:14 -0400

    RSP code and Project Name update

--- src/main.ts
+++ src/main.ts
@@ -1,1 +1,49 @@
-import "./style.css";
+import './style.css'
+
+// let p1 = 'rock'
+// let p2 = 'scissors'
+
+// const rps = (p1, p2) => {
+//   if (p1 == p2) return 'Draw!'
+
+//   if (p1 == 'rock' && p2 == 'scissors') return 'Player 1 won!'
+//   else if (p1 == 'scissors' && p2 == 'paper') return 'Player 1 won!'
+//   else if (p1 == 'paper' && p2 == 'rock') return 'Player 1 won!'
+//   else return 'Player 2 won!'
+// }
+
+const rps = (p1, p2) => {
+  if (p1 == 'scissors' && p2 == 'paper') {
+    console.log('Player 1 won!')
+  }
+  if (p1 == 'paper' && p2 == 'rock') {
+    console.log('Player 1 won!')
+  }
+  if (p1 == 'rock' && p2 == 'scissors') {
+    console.log('Player 1 won!')
+  }
+  if (p1 == 'paper' && p2 == 'scissors') {
+    console.log('Player 2 won!')
+  }
+  if (p1 == 'rock' && p2 == 'paper') {
+    console.log('Player 2 won!')
+  }
+  if (p1 == 'scissors' && p2 == 'rock') {
+    console.log('Player 2 won!')
+  }
+  if (p1 == 'scissors' && p2 == 'scissors') {
+    console.log('Draw!')
+  }
+  if (p1 == 'rock' && p2 == 'rock') {
+    console.log('Draw!')
+  }
+  if (p1 == 'paper' && p2 == 'paper') {
+    console.log('Draw!')
+  }
+}
+
+// rps('rock', 'scissors')
+// rps('scissors', 'scissors')
+// rps('paper', 'scissors')
+// rps('paper', 'paper')
+// rps('rock', 'rock')
