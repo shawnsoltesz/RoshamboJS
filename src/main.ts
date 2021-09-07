36d081eea37e51244eed1c10f82c1379acc21c4b
Shawn Soltesz <shawnjsoltesz@gmail.com>
2021-09-06 20:31:51 -0400

    updates to Scoring p1 and p2

--- src/main.ts
+++ src/main.ts
@@ -11,53 +11,98 @@ console.log(playerOneScoreDisplay)
 //Player 1 Options
 
 const playerOneRock = document.querySelector('.player1 .rock')
-console.log(playerOneRock)
+
+function handleClickOnPlayerOneRock() {
+  console.log('I clicked the rock')
+  let p1 = 'rock'
+  return p1
+}
+
+playerOneRock?.addEventListener('click', handleClickOnPlayerOneRock)
 
 const playerOnePaper = document.querySelector('.player1 .paper')
-console.log(playerOnePaper)
+function handleClickOnPlayerOnePaper() {
+  console.log('I clicked the paper')
+  let p1 = 'paper'
+  return p1
+}
+
+playerOnePaper?.addEventListener('click', handleClickOnPlayerOnePaper)
 
 const playerOneScissors = document.querySelector('.player1 .scissors')
-console.log(playerOneScissors)
+function handleClickOnPlayerOneScissors() {
+  console.log('I clicked the scissors')
+  let p1 = 'scissors'
+  return p1
+}
 
-// let p1 = 'rock'
-// let p2 = 'scissors'
+playerOneScissors?.addEventListener('click', handleClickOnPlayerOneScissors)
 
-// const rps = (p1, p2) => {
-//   if (p1 == p2) return 'Draw!'
+//Player 2 H3 Score
 
-//   if (p1 == 'rock' && p2 == 'scissors') return 'Player 1 won!'
-//   else if (p1 == 'scissors' && p2 == 'paper') return 'Player 1 won!'
-//   else if (p1 == 'paper' && p2 == 'rock') return 'Player 1 won!'
-//   else return 'Player 2 won!'
-// }
+const playerTwoScoreDisplay = document.querySelector('.player2 h3')
+console.log(playerTwoScoreDisplay)
 
-const rps = (p1, p2) => {
-  if (p1 == 'scissors' && p2 == 'paper') {
-    console.log('Player 1 won!')
-  }
-  if (p1 == 'paper' && p2 == 'rock') {
-    console.log('Player 1 won!')
-  }
-  if (p1 == 'rock' && p2 == 'scissors') {
-    console.log('Player 1 won!')
-  }
-  if (p1 == 'paper' && p2 == 'scissors') {
-    console.log('Player 2 won!')
-  }
-  if (p1 == 'rock' && p2 == 'paper') {
-    console.log('Player 2 won!')
-  }
-  if (p1 == 'scissors' && p2 == 'rock') {
-    console.log('Player 2 won!')
-  }
-  if (p1 == 'scissors' && p2 == 'scissors') {
-    console.log('Draw!')
-  }
-  if (p1 == 'rock' && p2 == 'rock') {
-    console.log('Draw!')
-  }
-  if (p1 == 'paper' && p2 == 'paper') {
-    console.log('Draw!')
+//Player 2 Options
+
+const playerTwoRock = document.querySelector('.player2 .rock')
+
+function handleClickOnPlayerTwoRock() {
+  console.log('I clicked the rock')
+  let p2 = 'rock'
+  return p2
+}
+
+playerTwoRock?.addEventListener('click', handleClickOnPlayerTwoRock)
+
+const playerTwoPaper = document.querySelector('.player2 .paper')
+function handleClickOnPlayerTwoPaper() {
+  console.log('I clicked the paper')
+  let p2 = 'paper'
+  return p2
+}
+
+playerTwoPaper?.addEventListener('click', handleClickOnPlayerTwoPaper)
+
+const playerTwoScissors = document.querySelector('.player2 .scissors')
+function handleClickOnPlayerTwoScissors() {
+  console.log('I clicked the scissors')
+  let p2 = 'scissors'
+  return p2
+}
+
+playerTwoScissors?.addEventListener('click', handleClickOnPlayerTwoScissors)
+
+//Scoring
+function Scoring(p1, p2) {
+  {
+    if (p1 == 'scissors' && p2 == 'paper') {
+      window.alert('Player 1 won!')
+    }
+    if (p1 == 'paper' && p2 == 'rock') {
+      window.alert('Player 1 won!')
+    }
+    if (p1 == 'rock' && p2 == 'scissors') {
+      window.alert('Player 1 won!')
+    }
+    if (p1 == 'paper' && p2 == 'scissors') {
+      window.alert('Player 2 won!')
+    }
+    if (p1 == 'rock' && p2 == 'paper') {
+      window.alert('Player 2 won!')
+    }
+    if (p1 == 'scissors' && p2 == 'rock') {
+      window.alert('Player 2 won!')
+    }
+    if (p1 == 'scissors' && p2 == 'scissors') {
+      window.alert('Draw!')
+    }
+    if (p1 == 'rock' && p2 == 'rock') {
+      window.alert('Draw!')
+    }
+    if (p1 == 'paper' && p2 == 'paper') {
+      window.alert('Draw!')
+    }
   }
 }
 
